ad9591157651820ffbf4a03a80e53d89953ed4a7
Carter Bosen <Carter.bosen99@gmail.com>
2023-05-17 14:07:05 -0600

    codealong

---
+++ prove/new/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Team awesomesauce = new Team("Awesome Sauce", 0, 0);
+
+Player neymar = new Player("Neymar jr.", 10, "Forward");
+Player Kobe = new Player("Kobe Bryant", 24);
+
+
+Kobe.SetPosition("Point Guard");
+
+
+awesomesauce.AddPlayer(Kobe);
+awesomesauce.AddPlayer(neymar);
+
+awesomesauce.AddWin();
+awesomesauce.AddLoss();
+
+awesomesauce.DisplayRoster();
+

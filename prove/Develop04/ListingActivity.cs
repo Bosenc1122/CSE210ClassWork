c8ce8ad380b351270766dd93a9369405b35777eb
Carter Bosen <Carter.bosen99@gmail.com>
2023-06-09 14:19:56 -0600

    finished Develop04

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -10,7 +10,8 @@ public class ListingActivity : Activity
     "What are personal strengths of yours?",
     "Who are people that you have helped this week?",
     "When have you felt the Holy Ghost this month?",
-    "Who are some of your personal heroes?"
+    "Who are some of your personal heroes?",
+    "Who are some people you have uplifted this week?"
     };
     private List<string> _userList = new List<string>();
     private string _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";

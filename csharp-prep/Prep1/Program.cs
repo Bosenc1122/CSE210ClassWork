270b44493bc0dd307e6e3091c670e8d81bba3696
Carter Bosen <Carter.bosen99@gmail.com>
2023-06-21 15:54:35 -0600

    Develop 05

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,8 +1,7 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{   static void Main(string[] args)
     {
        Console.Write("What is your first name? ");
         string first = Console.ReadLine();
